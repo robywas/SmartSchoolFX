d0f4ad9a9d698a4730f4e501e9be733270f69fb9
robywas <wasilewski.tg@gmail.com>
2018-10-07 23:50:28 +0200

    TopMenuButtons add methods

--- src/main/java/pl/myapp/SmartSchool/controllers/MainController.java
+++ src/main/java/pl/myapp/SmartSchool/controllers/MainController.java
@@ -1,7 +1,11 @@
 package pl.myapp.SmartSchool.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainController {
 
@@ -14,6 +18,17 @@ public class MainController {
     @FXML
     private void initialize() {
         topMenuButtonsController.setMainController(this);
+    }
+
+    public void setCenter(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(parent);
     }
 
 }
--- src/main/java/pl/myapp/SmartSchool/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/myapp/SmartSchool/controllers/TopMenuButtonsController.java
@@ -1,37 +1,54 @@
 package pl.myapp.SmartSchool.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
 
+    public static final String STUDENTS_FXML = "/fxml/Students.fxml";
+    public static final String TEACHERS_FXML = "/fxml/Teachers.fxml";
+    public static final String COURSES_FXML = "/fxml/Courses.fxml";
+    public static final String INCOMES_FXML = "/fxml/Incomes.fxml";
+    public static final String STATISTICS_FXML = "/fxml/Statistics.fxml";
+    public static final String ADD_STUDENT_FXML = "/fxml/AddStudent.fxml";
+
+
     private MainController mainController;
 
+    @FXML
+    ToggleGroup toggleButtons;
 
     @FXML
     public void openStudents() {
-        System.out.println("Students");
+        mainController.setCenter(STUDENTS_FXML);
     }
 
     @FXML
     public void openTeachers() {
-        System.out.println("Teachers");
+        mainController.setCenter(TEACHERS_FXML);
     }
 
     @FXML
     public void openCourses() {
-        System.out.println("Courses");
+        mainController.setCenter(COURSES_FXML);
     }
 
     @FXML
     public void openIncomes() {
-        System.out.println("Incomes");
+        mainController.setCenter(INCOMES_FXML);
 
     }
 
     @FXML
     public void openStats() {
-        System.out.println("Stats");
+        mainController.setCenter(STATISTICS_FXML);
+
+    }
+
+    @FXML
+    public void addStudent() {
+        mainController.setCenter(ADD_STUDENT_FXML);
 
     }
 
