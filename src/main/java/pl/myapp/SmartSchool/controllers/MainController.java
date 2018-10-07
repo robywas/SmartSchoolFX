bbae072e9a9f9759fae04b2768a9b5246c3b2c4c
robywas <wasilewski.tg@gmail.com>
2018-10-07 22:17:16 +0200

    TopMenuButtons

--- src/main/java/pl/myapp/SmartSchool/controllers/MainController.java
+++ src/main/java/pl/myapp/SmartSchool/controllers/MainController.java
@@ -1,4 +1,19 @@
 package pl.myapp.SmartSchool.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
+
+    @FXML
+    BorderPane borderPane;
+
+    @FXML
+    TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize() {
+        topMenuButtonsController.setMainController(this);
+    }
+
 }
--- src/main/java/pl/myapp/SmartSchool/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/myapp/SmartSchool/controllers/TopMenuButtonsController.java
@@ -1,4 +1,41 @@
 package pl.myapp.SmartSchool.controllers;
 
+import javafx.fxml.FXML;
+
 public class TopMenuButtonsController {
+
+
+    private MainController mainController;
+
+
+    @FXML
+    public void openStudents() {
+        System.out.println("Students");
+    }
+
+    @FXML
+    public void openTeachers() {
+        System.out.println("Teachers");
+    }
+
+    @FXML
+    public void openCourses() {
+        System.out.println("Courses");
+    }
+
+    @FXML
+    public void openIncomes() {
+        System.out.println("Incomes");
+
+    }
+
+    @FXML
+    public void openStats() {
+        System.out.println("Stats");
+
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 }
